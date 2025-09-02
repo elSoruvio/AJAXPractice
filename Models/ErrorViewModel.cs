f8f564d1d3305ddd1ad5411d8697596887b0589b
elSoruvio <144072510+elSoruvio@users.noreply.github.com>
2025-09-02 20:22:18 -0300

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AJAXPractice.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Models/TestData.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AJAXPractice.Models
+{
+  public class TestData
+  {
+    public string? MyProperty { get; set; }
+  }
+}
